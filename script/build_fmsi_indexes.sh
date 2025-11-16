043e148b344cc4d9c60eec7761fa4d13a032879c
Giulio Ermanno Pibiri <jeis90@gmail.com>
2025-11-16 14:43:58 +0100

    more scripting for k=63

--- script/build_fmsi_indexes.sh
+++ script/build_fmsi_indexes.sh
@@ -4,6 +4,8 @@ results_filename_prefix=$1
 
 source ~/miniconda3/bin/activate
 
+# k=31
+
 kmercamel compute -S -k 31 -M /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k31.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/cod.k31.eulertigs.fa.gz
 /usr/bin/time -v -a -o $results_filename_prefix.k31.build.time.log fmsi index -k 31 /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k31.msfa
 
@@ -21,3 +23,23 @@ kmercamel compute -S -k 31 -M /mnt/hd2/pibiri/DNA/masked-superstrings/se.k31.msf
 
 kmercamel compute -S -k 31 -M /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k31.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/hprc.k31.eulertigs.fa.gz
 /usr/bin/time -v -a -o $results_filename_prefix.k31.build.time.log fmsi index -k 31 /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k31.msfa
+
+# k=63
+
+kmercamel compute -S -k 63 -M /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k63.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/cod.k63.eulertigs.fa.gz
+/usr/bin/time -v -a -o $results_filename_prefix.k63.build.time.log fmsi index -k 63 /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k63.msfa
+
+kmercamel compute -S -k 63 -M /mnt/hd2/pibiri/DNA/masked-superstrings/kestrel.k63.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/kestrel.k63.eulertigs.fa.gz
+/usr/bin/time -v -a -o $results_filename_prefix.k63.build.time.log fmsi index -k 63 /mnt/hd2/pibiri/DNA/masked-superstrings/kestrel.k63.msfa
+
+kmercamel compute -S -k 63 -M /mnt/hd2/pibiri/DNA/masked-superstrings/human.k63.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/human.k63.eulertigs.fa.gz
+/usr/bin/time -v -a -o $results_filename_prefix.k63.build.time.log fmsi index -k 63 /mnt/hd2/pibiri/DNA/masked-superstrings/human.k63.msfa
+
+kmercamel compute -S -k 63 -M /mnt/hd2/pibiri/DNA/masked-superstrings/ncbi-virus.k63.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/ncbi-virus.k63.eulertigs.fa.gz
+/usr/bin/time -v -a -o $results_filename_prefix.k63.build.time.log fmsi index -k 63 /mnt/hd2/pibiri/DNA/masked-superstrings/ncbi-virus.k63.msfa
+
+kmercamel compute -S -k 63 -M /mnt/hd2/pibiri/DNA/masked-superstrings/se.k63.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/se.k63.eulertigs.fa.gz
+/usr/bin/time -v -a -o $results_filename_prefix.k63.build.time.log fmsi index -k 63 /mnt/hd2/pibiri/DNA/masked-superstrings/se.k63.msfa
+
+kmercamel compute -S -k 63 -M /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k63.msfa -o /dev/null /mnt/hd2/pibiri/DNA/eulertigs/hprc.k63.eulertigs.fa.gz
+/usr/bin/time -v -a -o $results_filename_prefix.k63.build.time.log fmsi index -k 63 /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k63.msfa
--- script/query_fmsi_indexes.sh
+++ script/query_fmsi_indexes.sh
@@ -2,6 +2,9 @@
 
 results_filename_prefix=$1
 
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DUSE_MAX_KMER_LENGTH_63=Off
+make -j
+
 ./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/cod.k31.canon.sshash
 ./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k31.msfa 2>> $results_filename_prefix.k31.bench.json
 ./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k31.msfa -q /mnt/hd2/pibiri/DNA/queries/SRR12858649.fastq.gz 2>> $results_filename_prefix.k31.streaming-query-high-hit.json
@@ -25,3 +28,33 @@ results_filename_prefix=$1
 ./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/hprc.k31.canon.sshash
 ./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k31.msfa 2>> $results_filename_prefix.k31.bench.json
 ./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k31.msfa -q /mnt/hd2/pibiri/DNA/queries/SRR5833294.fastq.gz 2>> $results_filename_prefix.k31.streaming-query-high-hit.json
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DUSE_MAX_KMER_LENGTH_63=On
+make -j
+
+./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/cod.k63.canon.sshash
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k63.msfa 2>> $results_filename_prefix.k63.bench.json
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/cod.k63.msfa -q /mnt/hd2/pibiri/DNA/queries/SRR12858649.fastq.gz 2>> $results_filename_prefix.k63.streaming-query-high-hit.json
+
+./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/kestrel.k63.canon.sshash
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/kestrel.k63.msfa 2>> $results_filename_prefix.k63.bench.json
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/kestrel.k63.msfa -q /mnt/hd2/pibiri/DNA/queries/SRR11449743_1.fastq.gz 2>> $results_filename_prefix.k63.streaming-query-high-hit.json
+
+./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/human.k63.canon.sshash
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/human.k63.msfa 2>> $results_filename_prefix.k63.bench.json
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/human.k63.msfa -q /mnt/hd2/pibiri/DNA/queries/SRR5833294.fastq.gz 2>> $results_filename_prefix.k63.streaming-query-high-hit.json
+
+./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/ncbi-virus.k63.canon.sshash
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/ncbi-virus.k63.msfa 2>> $results_filename_prefix.k63.bench.json
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/ncbi-virus.k63.msfa -q /mnt/hd2/pibiri/DNA/queries/ncbi-queries.fastq.gz 2>> $results_filename_prefix.k63.streaming-query-high-hit.json
+
+./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/se.k63.canon.sshash
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/se.k63.msfa 2>> $results_filename_prefix.k63.bench.json
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/se.k63.msfa -q /mnt/hd2/pibiri/DNA/queries/SRR27871075_1.fastq.gz 2>> $results_filename_prefix.k63.streaming-query-high-hit.json
+
+./sshash_benchmark -i /mnt/hd2/pibiri/DNA/sshash-indexes/hprc.k63.canon.sshash
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k63.msfa 2>> $results_filename_prefix.k63.bench.json
+./fmsi_benchmark -i /mnt/hd2/pibiri/DNA/masked-superstrings/hprc.k63.msfa -q /mnt/hd2/pibiri/DNA/queries/SRR5833294.fastq.gz 2>> $results_filename_prefix.k63.streaming-query-high-hit.json
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DUSE_MAX_KMER_LENGTH_63=Off
+make -j
